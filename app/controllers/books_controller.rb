3bae990efb149f77f239e9f9b43acea33f033d82
EC2 Default User <ec2-user@ip-172-31-7-228.ap-northeast-1.compute.internal>
2023-06-11 11:42:23 +0000

    最終

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,29 +2,52 @@ class BooksController < ApplicationController
   def new
     @Book = Book.new
   end
-  
+
   def create
-    
-    book = Book.new(book_params)
-    
-    book.save
-    
-    redirect_to '/top'
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Has been successfully"
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    book = Book.find(params[:id])  
+    book.destroy
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to '/books' 
   end
 
 private
 
  def book_params
   params.require(:book).permit(:title, :body)
- end 
- 
-end 
+ end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_09_060440) do
+ActiveRecord::Schema.define(version: 2023_06_10_121408) do
 
-  create_table "lists", force: :cascade do |t|
+  create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", precision: 6, null: false
